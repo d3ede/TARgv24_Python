3df2cd0b89a2352683c65b42217a8477ef4d162f
opilane <opilane@HP-CZC2349HT2>
2024-11-20 15:47:21 +0200

    Ulesanne 1

---
+++ TARgv24_Python.py
@@ -0,0 +1,6 @@
+#Ulesanne 1
+print("Hello World!")
+nimi=input("Mis on sinu nimi? ")
+print("Tere tulemast!", nimi)
+vanus=int(input("Kui vana sa oled? "))
+print("Tere tulemast! Tervitan sind", vanus)
