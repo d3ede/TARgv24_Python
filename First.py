0281902e9342fdeff3975877b26a0935642a4c14
d3ede <theskonel@gmail.com>
2025-01-29 15:38:09 +0200

    Add files via upload

---
+++ First.py
@@ -0,0 +1,33 @@
+openOrganization = "Open Organization: \n plusses: \n 1) flexible \n 2) inovations \n 3) communication with world"
+closeOrganization = "Close Organization: \n plusses: \n 1) Stability \n 2) more commercy \n 3) better security"
+
+print(openOrganization,closeOrganization)
+print("-"*15)
+commercialOrganization = "Examples: Sony, Microsoft, Amazon. \n - On võmalus saada kasu. \n - konkurets. \n - turg"
+noncommercialOrganization = "\n Examples: Punane rist, Greenpeace, Doctors Without Borders, Amnesty International"
+governmentOrganization = "\n Examples: Jails, omavalitsused. \n Rahastamine: maksud, riigielarve"
+educationalOrganization = "\n Examples: Koolid, ülikolid, tööstushariduskeskus. \n Rahastamine: riigielarve"
+startup = "\n Examples: Bolt, wise, pipedrive. \n Rahastamine: investeerimine"
+internationalOrganization = "\n Examples: NATO, OON.  \n Rahastamine: kõik liikmed maksavad märatud tasu"
+mediaOrganization = "\n Examples: ERR, Postimees, Delfi. \n Rahastamine: reklaam, subscription"
+religiosOrganization = "\n Examples: Katoliku kirik, õigeusu kirik. \n Rahastamine: riigielarve, toetused"
+associations = "\n Examples: Erasmus+ \n Rahastamine: riigielarve, investeerimine"
+fonds = "\n Examples: B&M Gatesi Säästud \n Rahastamine: riigielarve, investeerimine"
+print(commercialOrganization,noncommercialOrganization,governmentOrganization,educationalOrganization,startup,internationalOrganization,mediaOrganization,religiosOrganization,associations,fonds)
+print(closeOrganization.center(50, "-"))
+
+print ("Organization components")
+print("1) Hierarhia")
+print("2) Departeerimine")
+print("3) Komunikatsioonkaanalid")
+print("4) Roolid ja vastutused")
+print("5) Töövoog ja protsessid")
+print("6) Tööristad ja Tehnoloogiad")
+print("7) Kultuur")
+
+print("Ametlik suhtlemine")
+print("1) E-kiri, koosolekud ")
+print("Dokumentatsioon")
+print("1) Politikad")
+print("2) Juhised")
+print("3) Protokolid")
